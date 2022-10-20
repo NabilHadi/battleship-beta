c0a04a01b228d79c4f46f41462067efa726afec6
Nabil Hadi <napilfadl@gmail.com>
2022-10-20 08:08:32 +0200

    Add Gameboard factory function

---
+++ src/gameboard.js
@@ -0,0 +1,45 @@
+import { ship } from "./ship";
+
+export function gameboard(size) {
+  let board = [];
+  for (let x = 0; x < size; x++) {
+    board.push([]);
+    for (let y = 0; y < size; y++) {
+      board[x].push({ ship: null, isAttacked: false });
+    }
+  }
+
+  function placeShipAt(...coordinates) {
+    const s = ship(coordinates.length);
+    coordinates.forEach(([x, y]) => {
+      board[x][y].ship = s;
+    });
+  }
+
+  function receiveAttack(x, y) {
+    if (board[x][y].ship) {
+      board[x][y].ship.hit();
+      return true;
+    }
+    board[x][y].isAttacked = true;
+    return false;
+  }
+
+  function isAllShipsSunk() {
+    return board.every((row) => {
+      return row.every((col) => {
+        if (col.ship) {
+          return col.ship.isSunk();
+        }
+        return true;
+      });
+    });
+  }
+
+  return {
+    board,
+    placeShipAt,
+    receiveAttack,
+    isAllShipsSunk,
+  };
+}
---
+++ src/gameboard.test.js
@@ -0,0 +1,82 @@
+import { gameboard } from "./gameboard";
+import { describe, test, expect } from "@jest/globals";
+
+describe("Gameboard", () => {
+  test("Creates board with correct length", () => {
+    let g = gameboard(9);
+    expect(g.board[0][0]).toBeDefined();
+    expect(g.board[5][5]).toBeDefined();
+    expect(g.board[0][9]).not.toBeDefined();
+    expect(g.board[9]).not.toBeDefined();
+  });
+
+  test("Creates Board with correct intial properties", () => {
+    let g = gameboard(9);
+    for (let i = 0; i < g.board.length; i++) {
+      for (let j = 0; j < g.board[i].length; j++) {
+        expect(g.board[i][j].ship).toBeNull();
+        expect(g.board[0][0].isAttacked).toBe(false);
+      }
+    }
+  });
+
+  test("Calling placeShipAt function should update board state", () => {
+    let g = gameboard(9);
+    g.placeShipAt([0, 0], [0, 1], [0, 2]);
+    expect(g.board[0][0].ship).toBeDefined();
+    expect(g.board[0][1].ship).toBeDefined();
+    expect(g.board[0][2].ship).toBeDefined();
+    expect(g.board[1][1].ship).toBeNull();
+    expect(g.board[0][3].ship).toBeNull();
+  });
+
+  test("Calling receiveAttack function should update board state", () => {
+    let g = gameboard(9);
+    expect(g.board[0][0].isAttacked).toBe(false);
+    g.receiveAttack(0, 0);
+    expect(g.board[0][0].isAttacked).toBe(true);
+    expect(g.board[1][1].isAttacked).toBe(false);
+    g.receiveAttack(1, 1);
+    expect(g.board[1][1].isAttacked).toBe(true);
+  });
+
+  test("Calling receiveAttack function should determine if attack hit a ship", () => {
+    let g = gameboard(9);
+    g.placeShipAt([1, 1], [1, 2]);
+    expect(g.receiveAttack(1, 3)).toBe(false);
+    expect(g.receiveAttack(1, 1)).toBe(true);
+  });
+
+  test("attacking a ship should update ship state", () => {
+    let g = gameboard(9);
+
+    g.placeShipAt([1, 1], [1, 2]);
+    expect(g.board[1][1].ship.numOfHits).toBe(0);
+    expect(g.board[1][2].ship.numOfHits).toBe(0);
+
+    g.receiveAttack(1, 1);
+    expect(g.board[1][1].ship.numOfHits).toBe(1);
+    expect(g.board[1][2].ship.numOfHits).toBe(1);
+
+    g.receiveAttack(1, 2);
+    expect(g.board[1][1].ship.numOfHits).toBe(2);
+    expect(g.board[1][2].ship.numOfHits).toBe(2);
+
+    expect(g.board[1][1].ship.isSunk()).toBe(true);
+    expect(g.board[1][2].ship.isSunk()).toBe(true);
+  });
+
+  test("Should return correct isAllShipsSunk result", () => {
+    let g = gameboard(9);
+    expect(g.isAllShipsSunk()).toBe(true);
+
+    g.placeShipAt([2, 2], [3, 2]);
+    expect(g.isAllShipsSunk()).toBe(false);
+
+    g.receiveAttack(2, 2);
+    expect(g.isAllShipsSunk()).toBe(false);
+
+    g.receiveAttack(3, 2);
+    expect(g.isAllShipsSunk()).toBe(true);
+  });
+});
