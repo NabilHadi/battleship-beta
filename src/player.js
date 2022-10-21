c5d90025f7e50b1be6e57282b4ae38f2429a4cac
Nabil Hadi <napilfadl@gmail.com>
2022-10-21 15:37:49 +0200

    Add setGameboard function to player

--- src/player.js
+++ src/player.js
@@ -1,8 +1,14 @@
 import { v4 as uuidv4 } from "uuid";
 
 export function player(name) {
+  let gameboard;
+
   return {
     id: uuidv4(),
     name,
+    setGameboard(_gameboard) {
+      this.gameboard = _gameboard;
+    },
+    gameboard,
   };
 }
